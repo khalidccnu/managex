a66a34419e59f6c36491b95adfa575c9511caa76
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-07-11 07:58:28 +0600

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import brandIcon from "./favicon.png";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ManageX",
+  description:
+    "ManageX is a task management app to help stay organized and manage day-to-day.",
+  icons: {
+    shortcut: `${brandIcon.src}`,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" data-theme="cupcake">
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        {children}
+      </body>
+    </html>
+  );
+}
